17682dbf2e90c1db4271afefb442645285bad9e2
John Murphy <jd031988@gmail.com>
2021-12-24 06:59:55 +0000

    Add html,css,javascipt to the repo

---
+++ script.js
@@ -0,0 +1,19 @@
+const loveButton = document.querySelector('#loveBTN');
+
+const loveChance = Math.floor(Math.random() * 100 + 1);
+
+const letsFindLove = () => {
+	const nameOne = prompt('Who`s love life should we predict?');
+	const nameTwo = prompt(
+		`Who do we want to check ${nameOne}'s Chance of love with?`
+	);
+	if (!nameOne || !nameTwo) {
+		return alert('I need 2 names to be able to check the chance of love!');
+	} else {
+		return alert(
+			`The chance of everlasting love between ${nameOne} and ${nameTwo} is ${loveChance}%`
+		);
+	}
+};
+
+loveButton.addEventListener('click', letsFindLove);
